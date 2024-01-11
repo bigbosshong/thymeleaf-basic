58b99036f39761f27d8b859a819df906f5023277
bigbosshong1101 <bigbosshong1101@gmail.com>
2024-01-12 00:15:32 +0900

    유틸리티 객체와 날짜

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -63,11 +64,18 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+
     @Component("helloBean")
-    static class HelloBean{
+    static class HelloBean {
         public String hello(String data) {
             return "hello" + data;
         }
+    }
+
+    @GetMapping("date")
+    public String data(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
     }
 
     @Data
